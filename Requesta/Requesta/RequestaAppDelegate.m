3d44c066be9a3a660e07384ef9a79c2c5a844674
JonBrelje <jonathan.brelje@gmail.com>
2013-09-21 10:53:44 -0400

    Test
    I do to

--- Requesta/Requesta/RequestaAppDelegate.m
+++ Requesta/Requesta/RequestaAppDelegate.m
@@ -14,7 +14,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    
+    //This is my method now -Jon
     return YES;
 }
 							
