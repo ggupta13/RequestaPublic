786c64c09eba0274c153875e0f78584451d38800
JonBrelje <jonathan.brelje@gmail.com>
2013-09-21 12:58:14 -0400

    OpenScreenViewController

--- Requesta/Requesta/OpenScreenViewController.m
+++ Requesta/Requesta/OpenScreenViewController.m
@@ -35,25 +35,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)GoToDJSignIn:(id)sender {
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 1;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *simpleTableIdentifier = @"SimpleTableItem";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
-    
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
-    }
-    
-    cell.textLabel.text = @"string";
-    return cell;
+- (IBAction)DJSignInButtonPressed:(id)sender {
 }
 @end
